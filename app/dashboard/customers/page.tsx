ced8d72413c174de7e5384fad0b2b4ce0b2288e5
swu223 <sarahwu223@gmail.com>
2024-08-09 11:27:30 -0400

    pluralized invoice and customer

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,5 @@
 export default function Page() {
   return (
-  <p>Customer Page</p>
+  <p>Customers Page</p>
 );
 }
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,5 +1,5 @@
 export default function Page() {
   return (
-  <p>Invoice Page</p>
+  <p>Invoices Page</p>
 );
 }
