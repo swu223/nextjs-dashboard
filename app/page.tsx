85bab0b5e176f43b304b12e71bfca671fd70678f
swu223 <sarahwu223@gmail.com>
2024-08-15 13:11:02 -0400

    added styles back in

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import '@/app/ui/home.module.css';
+import styles from '@/app/ui/home.module.css';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
 
