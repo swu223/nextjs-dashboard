06306bb38433d55152b90202106e5065c4c052c1
swu223 <sarahwu223@gmail.com>
2024-08-20 10:20:49 -0400

    added search bar functionality and params and router to update the search params and router navigation

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,33 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { 
+  useSearchParams,
+  usePathname,
+  useRouter
+ } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  // Step 2a searchParams lets you read the searchParams of the search. this is written second
+  const searchParams = useSearchParams();
+  // step 3 add the pathname to capture the base pathname. add replace to navigate to the new page
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  
+  // Step 1 onClick and handlesearch written is first to test that the search is working. 
+  function handleSearch(term:string){
+    // Step 2b add the searchParams to capture the params
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      // if the search bar has a term, then we set the search params to the term
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    // Step 3b add the replace function to navigate
+    replace(`${pathname}?${params.toString()}`);
+  }
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +36,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e)=>{
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
