4a365aac525bd257395b4d2289bb99c0d5e81cd4
swu223 <sarahwu223@gmail.com>
2024-08-21 12:27:56 -0400

    error because customerID when needed customerId

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,9 @@
 'use server';
 
 import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -14,7 +17,7 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
-    customerID: formData.get('customerId'),
+    customerId: formData.get('customerId'),
     amount:     formData.get('amount'),
     status:     formData.get('status'),
   });
@@ -26,5 +29,11 @@ export async function createInvoice(formData: FormData) {
   // create date
   const date = new Date().toISOString().split('T')[0];
 
-  
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
