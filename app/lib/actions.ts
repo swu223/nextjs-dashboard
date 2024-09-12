6d84f00719418120841ff0fadd01a77a4a80e3c7
swu223 <sarahwu223@gmail.com>
2024-09-12 11:00:28 -0400

    added State and formvalidation to createInvoice in actions and added state and formaction to create-form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,28 +7,53 @@ import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending','paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.'
+  }),
+  amount: z.coerce.number()
+  .gt(0, {message: 'Please enter an amount greater than $0'}),
+  status: z.enum(['pending','paid'], {
+    invalid_type_error: 'Please select an invoice status.'
+  }),
   date: z.string(),
 })
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+  // Validate form fields using Zod
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount:     formData.get('amount'),
     status:     formData.get('status'),
   });
   // Note that you could do Object.fromEntries() to get all the fields of the form
-
+  console.log(validatedFields);
+  //If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice'
+    };
+  }
+  
+  // Prepare data for insertion
+  const { customerId, amount, status } = validatedFields.data;
   // convert all the amounts into cents
   const amountInCents = amount * 100;
-
   // create date
   const date = new Date().toISOString().split('T')[0];
 
+  // Insert data into the database
   try { 
     await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
@@ -40,6 +65,7 @@ export async function createInvoice(formData: FormData) {
     }
   }
 
+  // Revalidate the cache for the invoices page and redirect the user.
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
