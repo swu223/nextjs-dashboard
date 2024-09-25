4c10473e0877a985723ed2b4fd0c09e7f98b06d9
swu223 <sarahwu223@gmail.com>
2024-09-25 10:48:07 -0400

    added auth actions and middleware

--- auth.ts
+++ auth.ts
@@ -25,8 +25,8 @@ export const { auth, signIn, signOut } = NextAuth({
     Credentials({
       async authorize(credentials) {
         const parsedCredentials = z
-        .object({ email: z.string().email(), password: z.string().min(6) })
-        .safeParse(credentials);
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
 
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
